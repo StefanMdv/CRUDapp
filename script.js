b0887c30167d782326662aa2cb2fb59e936cabe9
StefanMdv <mdv.stefan@gmail.com>
2021-07-26 19:59:48 +0200

    end functions

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
+
+
 let names = ['Erica Mustermann', 'John Doe'];
 let phoneNumbers = ['015712345678', '015798765432'];
-
+load();
 function render() {
     let content = document.getElementById('content');
     content.innerHTML = ' ';
     content.innerHTML += ` <h1> My Contacts</h1>`;
+    content.innerHTML += `
+    <div>
+        <input placeholder="Name" id="name">
+        <input placeholder="Telefon" id="phone">
+        <button onclick="addContact()">Hinzufügen</button>
+    </div>
+    `;
 
-    for (let i = 0; i < names.length; i++){
+
+    for (let i = 0; i < names.length; i++) {
         const name = names[i];
         const phoneNumber = phoneNumbers[i];
         content.innerHTML += `
         <div class="card">
             <b>Name: </b> ${name}<br>
             <b>Telefon: </b> ${phoneNumber} <br>
+            <button onclick="deleteContact(${i})">Löschen</button>
         </div>
-        `; 
-        
+        `;
+
+    }
+}
+
+function addContact() {
+    let name = document.getElementById('name');
+    let phone = document.getElementById('phone');
+
+    names.push(name.value);
+    phoneNumbers.push(phone.value);
+
+    render();
+    save();
+}
+
+function deleteContact(i) {
+    names.splice(names[i], 1);
+    phoneNumbers.splice(phoneNumbers[i], 1);
+
+    render();
+    save();
+}
+function save(){
+    let namesAsText = JSON.stringify(names);
+    localStorage.setItem('names', namesAsText);
+    let phoneNumberAsText = JSON.stringify(phoneNumbers);
+    localStorage.setItem('phoneNumbers', phoneNumberAsText);
+}
+
+function load(){
+    let namesAsText = localStorage.getItem(names);
+    let phoneNumberAsText = localStorage.getItem(phoneNumbers);
+    
+    if(namesAsText && phoneNumberAsText){
+        names = JSON.parse(namesAsText);
+        phoneNumbers = JSON.parse(phoneNumberAsText);
     }
 }
